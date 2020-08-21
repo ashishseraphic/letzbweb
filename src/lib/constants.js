88870fab0113155df502cec3aa75629a3d70a7a2
Ashish Kumar <ashish@Apples-MacBook-Air.local>
2020-08-21 12:02:33 +0530

    changed DOMAIN_URL

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,9 @@
 export const Constant = {
   //  DOMAIN_URL: 'http://ec2-3-21-237-151.us-east-2.compute.amazonaws.com:5000/',
-  DOMAIN_URL: 'http://localhost:5000/',
+  // DOMAIN_URL: 'http://localhost:5000/',
+  DOMAIN_URL: "https://letzb.herokuapp.com/",
   ROLES: {
     SUPER_ADMIN: 0,
-    USER: 1
-  }
+    USER: 1,
+  },
 };
